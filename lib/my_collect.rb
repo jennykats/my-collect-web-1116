2b3356dba2d595a8d39cdf78b3ffef89a722bcd7
Jenny Kats <y.jennykats@gmail.com>
2016-11-17 02:46:38 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+# def my_collect (array)
+#   elements = []
+#     array.each do |lang|
+#         la = lang.split(" ").first
+#         elements << la.capitalize
+#   end
+#     return elements
+# end
 
 
+
+def my_collect(arr)
+  new_arr = []
+  i = 0
+  while i < arr.length
+    new_arr << (yield arr[i])
+    i += 1
+  end
+  new_arr
+end
